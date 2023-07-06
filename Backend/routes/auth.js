59dac2f0af104306a1070b36105583067c3ad5fd
Sreejanee <2105502@kiit.ac.in>
2023-07-07 02:54:16 +0530

    Creating the login endpoint

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -4,7 +4,7 @@ const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const JWT_SECRET="Thisisanoteskeepingwebsitemadewithreact"
+const JWT_SECRET = "Thisisanoteskeepingwebsitemadewithreact"
 
 const router = express.Router();
 // Create a user using POST "/api/auth/createuser", doesn't require authentication
@@ -22,8 +22,8 @@ router.post('/createuser', [
   try {
     //check whether an user with this email already exists.Othherwise promise resolve hone se pehle next line me chala jayega. so the salt variable takes the value of the promise and then goes to the next line
     //bcrypt.genSaltSync returns a promise so we have to use await
-    const salt =await bcrypt.genSaltSync(10);//creating salt from bcypt
-    const secPassword=await bcrypt.hash(req.body.password,salt);
+    const salt = await bcrypt.genSaltSync(10);//creating salt from bcypt
+    const secPassword = await bcrypt.hash(req.body.password, salt);
     const user = await User.create({
       name: req.body.name,
       email: req.body.email,
@@ -31,19 +31,19 @@ router.post('/createuser', [
     });
     //CREATING THE TOKEN
     // id of every document is used because id is an index. Indexes are used to quickly locate data without having to search every row in a database table every time said table is accessed.
-    const data={
-      user:{
-        id:user.id
+    const data = {
+      user: {
+        id: user.id
       }
     }
     //authToken which is given to each user after authentication can be recovered back in the above data to track the user
-    const authToken=jwt.sign(data,JWT_SECRET);
+    const authToken = jwt.sign(data, JWT_SECRET);
     //JWT_SECRET can be used to find out whether the data has been tempered or not
-    res.json({authToken});
+    res.json({ authToken });
   } catch (error) {
     if (error.code === 11000)//MongoDB Duplicate key error
-     {
-      
+    {
+
       return res.status(400).json({ error: 'Email already exists' });
     }
     console.error(error);
@@ -51,4 +51,48 @@ router.post('/createuser', [
   }
 });
 
+
+// Authenticate a user using POST "/api/auth/login", No login required
+router.post('/login', [
+  body('email', "Enter a valid Email").isEmail(),//if not valid email then we will not allow the user to go any further
+  body('password', "Passwords acnnot be blank").exists()
+
+], async (req, res) => {
+
+  //if there are errors return bad request and errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  //if there are no errors
+  const{email,password}=req.body;
+  try {
+    let user=await User.findOne({email})//checking whether the user exits with the email enterred by the client
+    if(!user){
+      return res.status(400).json({error:"Please try to login with the correct credentials"})
+    }
+    //COMPARE WHETHER RIGHT PASSWORD HAS BEEN ENTERED OR NOT
+    const passwordComp=await bcrypt.compare(password,user.password)//comparing the passowrd enterred by the client with the actual user passowrd from the database my matching it with the email
+    if(!passwordComp)//if the passwords dont match
+    {
+      return res.status(400).json({error:"Please try to login with the correct credentials"})
+    }
+    else{
+      //send the payload data
+      const data = {
+        user: {
+          id: user.id
+        }
+      }
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: 'Server error' });
+  }
+
+
+
+})
 module.exports = router;
