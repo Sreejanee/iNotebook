e471dd862e1cec04662fc43edb16c8099413b860
Sreejanee <2105502@kiit.ac.in>
2023-07-07 23:46:39 +0530

    fetching notes from Context

--- src/components/context/notes/NoteState.js
+++ src/components/context/notes/NoteState.js
@@ -2,10 +2,32 @@
 
 import NoteContext from "./noteContext";
 import { useState } from "react";
-const NoteState=(props)=>{
-    
-    return(
-        <NoteContext.Provider  >
+const NoteState = (props) => {
+    const notesInitial =
+        [
+            {
+                "_id": "64a8517a194391febba0fcf3",
+                "user": "64a77ec6d37109974f7b90bc",
+                "title": "My title-sree",
+                "description": "First note sree",
+                "tag": "Personally",
+                "date": "2023-07-07T17:55:06.214Z",
+                "__v": 0
+            },
+            {
+                "_id": "64a85190194391febba0fcf5",
+                "user": "64a77ec6d37109974f7b90bc",
+                "title": "My title",
+                "description": "First note ",
+                "tag": "Personal",
+                "date": "2023-07-07T17:55:28.745Z",
+                "__v": 0
+            }
+        ]
+        const[notes,setNotes]=useState(notesInitial)
+
+    return (
+        <NoteContext.Provider value={{notes,setNotes}} >
             {props.children}
         </NoteContext.Provider>
     )
